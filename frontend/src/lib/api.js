0f9ed1b5edddd069444e7e3b3266974b3240dee8
exdellsindia <chiranjeevprajapat@exdells.com>
2025-11-25 17:17:52 +0530

    fix: api.js file change

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,3 +1,5 @@
-import axios from 'axios'
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || '' })
-export default api
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://exdells-1.onrender.com",
+});
