25e3e5f02e8f377db4d83831e61747de51a515db
exdellsindia <chiranjeevprajapat@exdells.com>
2025-12-24 12:26:00 +0530

    commit changes index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,11 +1,10 @@
 const sequelize = require('../config/db');
-const User = require('./User');
-const Lead = require('./Lead');
-const Project = require('./Project');
+const UserFactory = require('./User');
+const LeadFactory = require('./Lead');
+const ProjectFactory = require('./Project');
 
-// register models (calling factory functions)
-User(sequelize);
-Lead(sequelize);
-Project(sequelize);
+const User = UserFactory(sequelize);
+const Lead = LeadFactory(sequelize);
+const Project = ProjectFactory(sequelize);
 
-module.exports = { sequelize };
+module.exports = { sequelize, User, Lead, Project };
