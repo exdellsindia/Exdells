fab9341f9f04e19cee4d88eebdeaa0fc449f347e
exdellsindia <chiranjeevprajapat@exdells.com>
2025-12-25 11:00:26 +0530

    cmmit changes and updated scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Chatbot from './components/Chatbot'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
@@ -17,6 +18,7 @@ import TermsAndConditions from './pages/TermsAndConditions'
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-white text-slate-800">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Routes>
